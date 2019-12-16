679a2d3277019eb407b8014c08530eb01ea9b032
Philipp Hoenisch <philipp@hoenisch.at>
2019-12-16 15:14:15 +1100

    Shutdown cnd nodes

--- api_tests/e2e/rfc003/btc_eth/refund.ts
+++ api_tests/e2e/rfc003/btc_eth/refund.ts
@@ -1,14 +1,30 @@
+import { Actor } from "../../../lib_sdk/actors/actor";
 import { AssetKind } from "../../../lib_sdk/asset";
 import { createActors } from "../../../lib_sdk/create_actors";
 
 setTimeout(function() {
+    let alice: Actor;
+    let bob: Actor;
+
+    beforeEach(async function() {
+        this.timeout(20000);
+        const actors = await createActors("e2e-rfc003-btc-eth-refund.log");
+        alice = actors.alice;
+        bob = actors.bob;
+    });
+
+    afterEach(() => {
+        if (alice) {
+            alice.stop();
+        }
+        if (bob) {
+            bob.stop();
+        }
+    });
+
     describe("Bob can refund and then Alice refunds", function() {
         this.timeout(60000);
         it("bitcoin ether", async function() {
-            const { alice, bob } = await createActors(
-                "e2e-rfc003-btc-eth-refund.log"
-            );
-
             await alice.sendRequest(AssetKind.Bitcoin, AssetKind.Ether);
             await bob.accept();
 
